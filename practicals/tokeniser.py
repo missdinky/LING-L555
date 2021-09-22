f8b5644c39b430747b838c336ef3f704afb6437d
drew shaeffer <89413591+missdinky@users.noreply.github.com>
2021-09-22 13:48:59 -0400

    Update tokeniser.py

--- practicals/tokeniser.py
+++ practicals/tokeniser.py
@@ -4,7 +4,7 @@ text2 = sys.stdin.readlines()
 
 #separate punctuation from the text with spaces
 for line in text2:
-	for c in "'.:])(":
+	for c in "',.:])(":
 		line = line.replace(c, " " + c + " ")
 	#split the spaces into new lines
 	tokens = line.split(" ")
